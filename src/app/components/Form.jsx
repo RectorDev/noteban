b26f5892129e7525d3dc1a78eab039660b1f2f2c
RectorDev <Rector.dev@gmail.com>
2024-10-01 16:16:33 +0330

    initial commit

---
+++ src/app/components/Form.jsx
@@ -0,0 +1,23 @@
+const Form = () => {
+  return (
+    <Form onclick>
+      <label htmlFor="todo">
+        <input
+          type="text"
+          name="todo"
+          id="todo"
+          placeholder="Write your nxt task"
+        />
+      </label>
+
+      <button>
+        <span>Submit</span>
+        <svg>
+          <path d="" />
+        </svg>
+      </button>
+    </Form>
+  );
+};
+
+export default Form;
