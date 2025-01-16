626a749e522d1d9001a1ffdc070eb588b05fb583
RectorDev <Rector.dev@gmail.com>
2025-01-16 21:25:29 +0330

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,30 @@
 'use client'
 import './page.css'
-import { Typewriter } from 'react-simple-typewriter'
+import {Typewriter} from 'react-simple-typewriter'
+import {Button} from "@/components/ui/button";
+import {MailOpen} from "lucide-react";
+import React from "react";
+import Image from "next/image";
 
 export default function Home() {
-    return (<div className="background max-container padding relative">
+    return (<div className="mx-auto my-0 max-w-screen-xl mt-10 padding ">
+        <div className={"relative flex justify-between  items-center gap-4 max-lg:flex-col"}>
+            <div>
+                <h1 className={'text-2xl ml-1 font-press text-nowrap max-lg:text-wrap'}>Hello im, <span className="absolute max-lg:relative"><Typewriter
+                    words={["Rector", 'a Web Developer']}
+                    loop={true} cursor={true}/></span></h1>
+                <p className={'font-roboto my-10  font-[400]'}>I craft responsive Websites where technologies meet
+                    creativity</p>
+                <Button className={"bg-primary"}><MailOpen/>Contact me</Button>
 
-        <h1 className={'text-5xl font-press'}>Hello im,<Typewriter words={["Rector",'a Web Developer']} loop={true} cursor={true}/></h1>
+            </div>
+
+            <div className={"relative flex justify-center items-end"}>
+                <Image className={"object-contain"} width={350} height={350} src={"/avatar.png"} alt="Brain Rot"/>
+                <div className={"border-border border w-full flex items-center px-1 pos-center -bottom-9"}><span
+                    className={"h-4 w-4 bg-border inline-block"}></span> &#xa0;Currently working on <span
+                    className={"font-bold text-amber-500"}>&#xa0; Nothing</span></div>
+            </div>
+        </div>
     </div>);
 }
